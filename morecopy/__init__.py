68396207ec2d7f8ee4fa63889ba5e505b6ff2ce3
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2023-07-15 13:30:08 +0000

    #31 Update package root

--- morecopy/__init__.py
+++ morecopy/__init__.py
@@ -1,12 +1,13 @@
-# type: ignore
+__all__ = [
+    "copy",
+    "copiers",
+    "copier_for",
+]
 __version__ = "0.2.4"
 
 
 # submodules
 from . import copy
 from . import copiers
-
-
-# aliases
 from .copy import *
 from .copiers import *
